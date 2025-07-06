398e6079df2f32f4980542eb6b37b92ba055a4e2
wronboy <agus.handoko.smaga@gmail.com>
2025-07-06 16:01:54 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MyCvonAWS.Models;
+
+namespace MyCvonAWS.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _configuration;
+
+    public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+    }
+
+    public IActionResult Index()
+    {
+        var model = _configuration.GetSection("CvData").Get<CvViewModel>();
+        return View(model);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ Models/CvViewModel.cs
@@ -0,0 +1,46 @@
+﻿namespace MyCvonAWS.Models
+{
+    public class CvViewModel
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Email { get; set; }
+        public string LinkedIn { get; set; }
+        public string Summary { get; set; }
+        public List<WorkExperience> WorkExperiences { get; set; }
+        public List<Project> Projects { get; set; }
+        public List<Education> Educations { get; set; } = new();
+        public List<Skill> ProgrammingSkills { get; set; } = new();
+        public List<Skill> DatabaseSkills { get; set; } = new();
+        public List<Skill> ToolSkills { get; set; } = new();
+    }
+
+    public class WorkExperience
+    {
+        public string Company { get; set; }
+        public string Period { get; set; }
+        public List<string> Responsibilities { get; set; }
+    }
+
+    public class Project
+    {
+        public string Name { get; set; }
+        public string Period { get; set; }
+        public List<string> Details { get; set; }
+    }
+
+    public class Education
+    {
+        public string Institution { get; set; }
+        public string Degree { get; set; }
+        public string Period { get; set; }
+        //public List<string> Details { get; set; }
+    }
+
+    public class Skill
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Level { get; set; } // e.g., 0-100 for percentage
+    }
+
+}
